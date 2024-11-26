b6b7f60ee81d62d42c8c02e3b37c008f41151bb2
Robson Cesar de Siqueira <robson.desiqueira@playstudios.com>
2024-11-26 13:15:36 -0300

    Initial commit

---
+++ SwiftUIKitCombineWithTests/SwiftUIKitCombineWithTests/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftUIKitCombineWithTests
+//
+//  Created by Robson Cesar de Siqueira on 26/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SwiftUIKitCombineWithTests/SwiftUIKitCombineWithTestsTests/SwiftUIKitCombineWithTestsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIKitCombineWithTestsTests.swift
+//  SwiftUIKitCombineWithTestsTests
+//
+//  Created by Robson Cesar de Siqueira on 26/11/24.
+//
+
+import Testing
+@testable import SwiftUIKitCombineWithTests
+
+struct SwiftUIKitCombineWithTestsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
